da5ecf7213647bf5336cad076bf8eaf2675fd7ef
xide.xu <37680625+Otto-Xu@users.noreply.github.com>
2023-07-27 11:06:34 +0800

    Update Rect.c

--- OpenCv/LearningOpenCv3/3. Getting to Know OpenCv Data Types/Rect.c
+++ OpenCv/LearningOpenCv3/3. Getting to Know OpenCv Data Types/Rect.c
@@ -13,13 +13,28 @@ int main()
 		return -1;
 	}
 
+	//值构造函数
 	Rect r1(100, 100, 300, 300);
-	Rect r2(Point(0, 0), Point(200, 200));
-	Rect r3(Point(250, 250), Size(200, 200));
-
 	rectangle(src, r1, Scalar(255, 0, 0));
+
+	//由两个对角构造
+	Rect r2(Point(0, 0), Point(200, 200));
 	rectangle(src, r2, Scalar(0, 255, 0));
+
+	//由起始点和大小构造
+	Rect r3(Point(250, 250), Size(200, 200));
 	rectangle(src, r3, Scalar(0, 0, 255));
+
+	//平移矩形r1
+	Point p1(50, 50);
+	Rect r4 = r1 + p1;
+	rectangle(src, r4, Scalar(255, 0, 255));
+	imshow(path, src);
+
+	//扩大矩形r1
+	Size s1(50, 50);
+	Rect r5 = r1 + s1;
+	rectangle(src, r5, Scalar(0, 255, 255));
 
 	imshow(path, src);
 	waitKey();
