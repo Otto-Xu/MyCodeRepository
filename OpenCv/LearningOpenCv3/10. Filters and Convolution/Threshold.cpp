b18f0b9343d847c3c1b153930b3c208b83ebfcf2
xide.xu <37680625+Otto-Xu@users.noreply.github.com>
2023-08-07 15:19:42 +0800

    Update Threshold.cpp

--- OpenCv/LearningOpenCv3/10. Filters and Convolution/Threshold.cpp
+++ OpenCv/LearningOpenCv3/10. Filters and Convolution/Threshold.cpp
@@ -3,21 +3,61 @@
 using namespace std;
 using namespace cv;
 
+/*
+	double cv::threshold(
+		cv::InputArray		src,			// Input image
+		cv::OutputArray		dst,			// Result image
+		double				thresh,			// Threshold value
+		double				maxValue,		// Max value for upward operations
+		int					thresholdType	// Threshold type to use (Example 10-3)
+	);
+
+	void cv::adaptiveThreshold(
+		cv::InputArray		src,			// Input image
+		cv::OutputArray		dst,			// Result image
+		double				maxValue,		// Max value for upward operations
+		int					adaptiveMethod, // mean or Gaussian
+		int					thresholdType,	// Threshold type to use (Example 10-3)
+		int					blockSize,		// Block size
+		double				C				// Constant
+	);
+
+	thresholdType的可选项
+	cv::THRESH_BINARY		| DST = (SRC > thresh) ? MAXVALUE : 0
+	cv::THRESH_BINARY_INV	| DST = (SRC > thresh) ? 0 : MAXVALUE
+	cv::THRESH_TRUNC		| DST = (SRC > thresh) ? THRESH : SRC
+	cv::THRESH_TOZERO		| DST = (SRC > thresh) ? SRC : 0
+	cv::THRESH_TOZERO_INV	| DST = (SRC > thresh) ? 0 : SRC
+
+	cv::adaptiveThreshold()根据adaptiveMethod的设置，允许两种不同的自适应阙值方法。
+	两种方法都是逐个像素地计算自适应阙值T(x,y)，方法是通过计算每个像素位置周围的b x b区域的加权平均值然后减去常数C，其中b由blocksize给定。
+	相对于一般的阈值化操作，当图像中出现较大的明暗差异时，自适应阈值时非常有效。
+*/
+
 int main()
 {
-	string path = "C:\\Users\\xuxide\\Desktop\\opencv\\Lenna.jpg";
-	Mat src = imread(path, IMREAD_GRAYSCALE);
-	if (src.empty())
+	string path = "C:\\Users\\xuxide\\Desktop\\opencv\\cat.jpg";
+	Mat Igray = imread(path, IMREAD_GRAYSCALE);
+	if (Igray.empty())
 	{
 		cout << "can not load " << path << endl;
 		return -1;
 	}
-	imshow(path,  src);
 
-	Mat dst;
-	threshold(src, dst, 127, 255, THRESH_BINARY);
-	imshow("dst", dst);
+	double fixed_thrshold = 127;
+	int threshold_type = THRESH_BINARY;
+	int adaptive_method = ADAPTIVE_THRESH_GAUSSIAN_C;
+	int block_size = 5;
+	double offset = 0;
+
+	Mat It, Iat;
+	threshold(Igray, It, fixed_thrshold, 255, threshold_type);
+	adaptiveThreshold(Igray, Iat, 255, adaptive_method, threshold_type, block_size, offset);
+
+	imshow("Raw", Igray);
+	imshow("Threshold", It);
+	imshow("Adaptive Threshold", Iat);
 
 	waitKey(0);
-    return 0;
+	return 0;
 }
