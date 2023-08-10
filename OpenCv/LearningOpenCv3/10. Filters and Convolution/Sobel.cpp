07f784d6d67a8f10562299d243eeb56170777a33
xide.xu <37680625+Otto-Xu@users.noreply.github.com>
2023-08-10 16:48:11 +0800

    Update Sobel.cpp

--- OpenCv/LearningOpenCv3/10. Filters and Convolution/Sobel.cpp
+++ OpenCv/LearningOpenCv3/10. Filters and Convolution/Sobel.cpp
@@ -4,13 +4,13 @@ using namespace std;
 using namespace cv;
 
 /*
-void cv::threshold(
+void cv::Sobel(
 	cv::InputArray		src,			// Input image
 	cv::0utputArray		dst,			// Result image
 	int			ddepth,			// Pixel depth of output(e.g., CV_8U)
 	int			xorder,			// order of corresponding derivative in x
 	int			yorder,			// order of corresponding derivative in y
-	cv::Size		ksize = 3,		// Kernel size 
+	cv::Size		ksize = 3,		// Kernel size
 	double			scale = 1,		// Scale(applied before assignment)
 	double			delta = 0，		// Offset(applied before assignment)
 	int			broderType		// Border extrapolation
@@ -23,7 +23,7 @@ ksize是一个奇数，表示调用的滤波器的宽和高。
 int main()
 {
 	string path = "C:\\Users\\xuxide\\Desktop\\opencv\\cat.jpg";
-	Mat src = imread(path), dst;
+	Mat src = imread(path, IMREAD_GRAYSCALE), dst;
 	if (src.empty())
 	{
 		cout << "can not load " << path << endl;
@@ -31,17 +31,17 @@ int main()
 	}
 	imshow(path, src);
 
+	Sobel(src, dst, CV_8U, 1, 0, 3, 1, 0, BORDER_DEFAULT);
+	imshow("x_dst", dst);
+
+	Sobel(src, dst, CV_8U, 0, 1, 3, 1, 0, BORDER_DEFAULT);
+	imshow("y_dst", dst);
+
 	Sobel(src, dst, CV_8U, 1, 1, 3, 1, 0, BORDER_DEFAULT);
 	imshow("dst1", dst);
 
-	Sobel(src, dst, CV_8U, 1, 0, 3, 1, 0, BORDER_DEFAULT);
-	imshow("dst2", dst);
-
-	Sobel(src, dst, CV_8U, 0, 1, 3, 1, 0, BORDER_DEFAULT);
-	imshow("dst3", dst);
-
 	Sobel(src, dst, CV_8U, 1, 1, 5, 1, 0, BORDER_DEFAULT);
-	imshow("dst4", dst);
+	imshow("dst2", dst);
 
 	waitKey(0);
 	return 0;
