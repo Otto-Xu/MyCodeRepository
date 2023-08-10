00e28effa1c885521a5d357e8cb1fff3b49dc6b6
xide.xu <37680625+Otto-Xu@users.noreply.github.com>
2023-08-10 15:27:18 +0800

    Update GaussianBlur.cpp

--- OpenCv/LearningOpenCv3/10. Filters and Convolution/GaussianBlur.cpp
+++ OpenCv/LearningOpenCv3/10. Filters and Convolution/GaussianBlur.cpp
@@ -16,19 +16,32 @@ void cv::GaussianBlur(
 
 int main()
 {
-	string path = "C:\\Users\\xuxide\\Desktop\\opencv\\hand1.jpg";
-	Mat src = imread(path), dst;
+	float data[9] = {9, 5, 1, 8, 4, 7, 6, 2, 3};
+	Mat m0(3, 3, CV_32FC1, data);
+	cout << "m0 = \n" <<  m0 << endl;
+
+	Mat m1;
+	GaussianBlur(m0, m1, cv::Size(3, 3), 1, 1, BORDER_DEFAULT);
+	cout << "m1 = \n" << m1 << endl;
+
+	GaussianBlur(m0, m1, cv::Size(3, 3), 0.5, 0.5, BORDER_DEFAULT);
+	cout << "m1 = \n" << m1 << endl;
+
+	GaussianBlur(m0, m1, cv::Size(3, 3), 10, 10, BORDER_DEFAULT);
+	cout << "m1 = \n" << m1 << endl;
+
+	string path = "C:\\Users\\xuxide\\Desktop\\opencv\\Lenna.jpg";
+	Mat src = imread(path, IMREAD_GRAYSCALE), dst;
 	if (src.empty())
 	{
 		cout << "can not load " << path << endl;
 		return -1;
 	}
 	imshow(path, src);
-
-	GaussianBlur(src, dst, cv::Size(5, 3), 1, 0.5, BORDER_DEFAULT);
+	GaussianBlur(src, dst, cv::Size(5, 3), 1, 1, BORDER_DEFAULT);
 	imshow("blur1", dst);
 
-	GaussianBlur(src, dst, cv::Size(5, 5), 0.0, 0.0, BORDER_DEFAULT);
+	GaussianBlur(src, dst, cv::Size(5, 5), 20, 20, BORDER_DEFAULT);
 	imshow("blur2", dst);
 
 	waitKey(0);
