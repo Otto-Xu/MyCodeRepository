8b6c37748d4e7277abf26005b92fa2776b831fad
xide.xu <37680625+Otto-Xu@users.noreply.github.com>
2023-08-15 15:08:44 +0800

    Update MorphologyEx.cpp

--- OpenCv/LearningOpenCv3/10. Filters and Convolution/MorphologyEx.cpp
+++ OpenCv/LearningOpenCv3/10. Filters and Convolution/MorphologyEx.cpp
@@ -4,7 +4,7 @@ using namespace std;
 using namespace cv;
 
 /*
-void cv::erode(
+void cv::morphologyEx(
 	cv::InputArray		src,							// Input image
 	cv::0utputArray		dst,							// Result image
 	int			op,							// Operator(e.g. MORPH_OPEN)
@@ -12,10 +12,10 @@ void cv::erode(
 	cv::Point		anchor		= cv::Point(-1,-1),			// Location of anchor point
 	int			iterations	= 1，					// Number of times to apply
 	int			borderType	= cv::BORDER CONSTANT			// Border extrapolation
-	const cv::Scalar&	borderValue = cv::morphologyDefaultBorderValue()
+	const cv::Scalar&	borderValue     = cv::morphologyDefaultBorderValue() 
 );
 
-MORPH_OPEN		开操作，先腐蚀再膨胀（两次开操作，腐蚀腐蚀膨胀膨胀）
+MORPH_OPEN		开操作，先腐蚀再膨胀（两次开操作，操作顺序是腐蚀腐蚀膨胀膨胀）
 MORPH_CLOSE		闭操作，先膨胀再腐蚀
 MORPH_TOPHAT		顶帽操作，TopHat(src) = src - open(src)		//Isolate brighter，得到了比源图像更亮的环绕部分
 MORPH_BLACKHAT		底帽操作，BlackHat(src) = close(src) - src	//Isolate dimmer ， 得到了比源图像更暗的环绕部分
@@ -24,7 +24,7 @@ MORPH_GRADIENT		形态学梯度，gradient(src) = dilate(src) - erode(src)
 
 int main()
 {
-	string path = "C:\\Users\\Administrator\\Desktop\\OpenCv\\Lenna.jpg";
+	string path = "C:\\Users\\xuxide\\Desktop\\opencv\\hand1.jpg";
 	Mat src = imread(path), src_bin, dst;
 	if (src.empty())
 	{
@@ -32,22 +32,22 @@ int main()
 		return -1;
 	}
 
-	Laplacian(src, src_bin, CV_8U, 3, 1, 0, BORDER_DEFAULT);
-	imshow(path, src_bin);
+	//Laplacian(src, src_bin, CV_8U, 3, 1, 0, BORDER_DEFAULT);
+	imshow(path, src);
 
-	morphologyEx(src_bin, dst, MORPH_OPEN, cv::Mat(), cv::Point(-1, -1), 1, BORDER_CONSTANT, cv::morphologyDefaultBorderValue());
+	morphologyEx(src, dst, MORPH_OPEN, cv::Mat(), cv::Point(-1, -1), 1, BORDER_CONSTANT, cv::morphologyDefaultBorderValue());
 	imshow("dst1", dst);
 
-	morphologyEx(src_bin, dst, MORPH_CLOSE, cv::Mat(), cv::Point(-1, -1), 1, BORDER_CONSTANT, cv::morphologyDefaultBorderValue());
+	morphologyEx(src, dst, MORPH_CLOSE, cv::Mat(), cv::Point(-1, -1), 1, BORDER_CONSTANT, cv::morphologyDefaultBorderValue());
 	imshow("dst2", dst);
 
-	morphologyEx(src_bin, dst, MORPH_TOPHAT, cv::Mat(), cv::Point(-1, -1), 1, BORDER_CONSTANT, cv::morphologyDefaultBorderValue());
+	morphologyEx(src, dst, MORPH_TOPHAT, cv::Mat(), cv::Point(-1, -1), 1, BORDER_CONSTANT, cv::morphologyDefaultBorderValue());
 	imshow("dst3", dst);
 
-	morphologyEx(src_bin, dst, MORPH_BLACKHAT, cv::Mat(), cv::Point(-1, -1), 1, BORDER_CONSTANT, cv::morphologyDefaultBorderValue());
+	morphologyEx(src, dst, MORPH_BLACKHAT, cv::Mat(), cv::Point(-1, -1), 1, BORDER_CONSTANT, cv::morphologyDefaultBorderValue());
 	imshow("dst4", dst);
 
-	morphologyEx(src_bin, dst, MORPH_GRADIENT, cv::Mat(), cv::Point(-1, -1), 1, BORDER_CONSTANT, cv::morphologyDefaultBorderValue());
+	morphologyEx(src, dst, MORPH_GRADIENT, cv::Mat(), cv::Point(-1, -1), 1, BORDER_CONSTANT, cv::morphologyDefaultBorderValue());
 	imshow("dst5", dst);
 
 	waitKey(0);
