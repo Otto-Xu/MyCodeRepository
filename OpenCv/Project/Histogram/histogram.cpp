e4c7b03c24c53a90362ea1d69b63ca37dfd93fa5
xide.xu <37680625+Otto-Xu@users.noreply.github.com>
2023-10-08 16:35:52 +0800

    Update histogram.cpp

--- OpenCv/Project/Histogram/histogram.cpp
+++ OpenCv/Project/Histogram/histogram.cpp
@@ -17,24 +17,26 @@ int main()
 	split(img, bgr);
 
 	int numbins = 256;
-
-	float range[] = { 0, 256 };
-	const float* histRange = { range };
+	float range[] = { 0, 255 };//像素点的取值范围
+	const float* histRange = { range };//方便传参
 
 	Mat b_hist, g_hist, r_hist;
-
+	//计算得到直方图数据
 	calcHist(&bgr[0], 1, 0, Mat(), b_hist, 1, &numbins, &histRange);
 	calcHist(&bgr[1], 1, 0, Mat(), g_hist, 1, &numbins, &histRange);
 	calcHist(&bgr[2], 1, 0, Mat(), r_hist, 1, &numbins, &histRange);
 
+	//设置直方图画布的参数，直方图要画在一个画布上，这个画布需要设置大小
 	int width = 512;
 	int height = 300;
 	Mat histImage(height, width, CV_8UC3, Scalar(20, 20, 20));
 
+	//对三个通道的直方图数据进行归一化处理，这是一个必要环节
 	normalize(b_hist, b_hist, 0, height, NORM_MINMAX);
 	normalize(g_hist, g_hist, 0, height, NORM_MINMAX);
 	normalize(r_hist, r_hist, 0, height, NORM_MINMAX);
 
+	//以折现统计图的方式绘制三个通道的直方图在画布上
 	int binStep = cvRound((float)width / (float)numbins);
 	for (int i = 1; i < numbins; i++)
 	{
@@ -55,6 +57,7 @@ int main()
 		);
 	}
 
+	//显示
 	imshow("Histogram", histImage);
 
 	waitKey(0);
